f578c6a38eccb262e3539a25d3c1e7b6abf155ee
Kagwi <kelvinmwas8@gmail.com>
2025-09-30 13:25:08 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -135,18 +135,7 @@ const Header = () => {
         </div>
 
         {/* Mobile Menu */}
-        {isMenuOpen && (
-          <div className="md:hidden absolute top-full left-0 right-0 bg-white shadow-xl border-t border-gray-100">
-            <div className="p-4 space-y-4">
-              <div className="relative mb-4">
-                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
-                <input
-                  type="text"
-                  placeholder="Search products..."
-                  className="w-full pl-10 pr-4 py-2 rounded-full border border-gray-200 focus:border-green-500 focus:ring-2 focus:ring-green-200 focus:outline-none"
-                />
-              </div>
-              
+        {isMenuOpen && (    
               {['Home', 'Products', 'About', 'Services', 'Gallery', 'FAQ', 'Contact'].map((item) => (
                 <Link
                   key={item}
